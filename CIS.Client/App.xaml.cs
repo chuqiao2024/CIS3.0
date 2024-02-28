3a5fbe2dbef8046dd2604e7567f3c359d85006ce
chuqiao <1940043363@qq.com>
2024-02-28 15:47:41 +0800

    添加项目文件。

---
+++ CIS.Client/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CIS.Client.ViewModels;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CIS.Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainWindow, MainWindowViewModel>();
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+            //moduleCatalog.AddModule<OperationModule>();
+        }
+    }
+
+}
